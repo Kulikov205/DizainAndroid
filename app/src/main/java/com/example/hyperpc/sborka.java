915c459ce3ab23bbedc8621c563894ba8ef59d12
dozopravka <dozopravka@example.com>
2023-03-27 20:34:39 +0300

    sborka

--- app/src/main/java/com/example/hyperpc/sborka.java
+++ app/src/main/java/com/example/hyperpc/sborka.java
@@ -1,10 +1,18 @@
 package com.example.hyperpc;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class sborka extends AppCompatActivity {
 
@@ -12,6 +20,22 @@ public class sborka extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sborka);
+
+
+
+        //com.gamma.scan
+        //com.gamma.barcodeapp.ui.BarcodeCaptureActivity
+        Button button = findViewById(R.id.scaning);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                intent.setComponent(new ComponentName("com.gamma.scan", "com.gamma.barcodeapp.ui.BarcodeCaptureActivity"));
+                startActivity(intent);
+            }
+        });
     }
 
     public void goBack(View v){
